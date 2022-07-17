7bf92c61558b6005a05424be7581ec0557a1fa6a
gregorylira <gregory.lira007@gmail.com>
2022-07-17 14:14:03 -0300

    .

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ const envs = dotenv.config();
 envs.error && new Error("Couldn't find .env file");
 
 export default {
-  port_server: Number(process.env.PORT_SERVER),
+  port_server: Number(process.env.PORT),
   bucket_name: process.env.BUCKET_NAME,
   mongo_url: process.env.MONGO_URL,
   storage_type: process.env.STORAGE_TYPE,
